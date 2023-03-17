843620b10fda2c7daf6e113760aae9bf9144f41a
EnclaveX <contatoenclave@gmail.com>
2023-03-16 22:20:17 -0300

    ajuste cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -23,6 +23,7 @@ const app = express();
 app.use(cors());
 
 app.use((req, res, next) => {
+    req.header("Access-Control-Allow-Origin");
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "*");
     res.header("Access-Control-Allow-Methods", "*");
