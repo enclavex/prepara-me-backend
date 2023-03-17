8b9af72771d3207d36af359a8d14fd1999c6804a
EnclaveX <contatoenclave@gmail.com>
2023-03-16 22:53:51 -0300

    ajuste cors

--- src/shared/infra/http/routes/autheticate.routes.ts
+++ src/shared/infra/http/routes/autheticate.routes.ts
@@ -1,13 +1,14 @@
 import { AuthenticateUserController } from "@modules/accounts/useCases/authenticateUser/AuthenticateUserController";
 import { RefreshTokenController } from "@modules/accounts/useCases/refreshToken/RefreshTokenController";
 import { Router } from "express";
+import cors from "cors";
 
 const authenticateRoutes = Router();
 
 const authenticateUserController = new AuthenticateUserController();
 const refreshTokenController = new RefreshTokenController();
 
-authenticateRoutes.post("/sessions", authenticateUserController.handle);
+authenticateRoutes.post("/sessions", cors(), authenticateUserController.handle);
 authenticateRoutes.post("/refreshToken", refreshTokenController.handle);
 
 export { authenticateRoutes };
