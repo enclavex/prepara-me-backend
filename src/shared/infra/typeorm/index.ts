5dbb5912e2df51c0b93b1593f7c4f2a0302cf1c9
EnclaveX <contatoenclave@gmail.com>
2023-03-18 14:11:49 -0300

    voltando

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -3,6 +3,8 @@ import { Connection, createConnection, getConnectionOptions } from "typeorm";
 export default async (): Promise<Connection> => {
     const defaultOptions = await getConnectionOptions();
 
+    console.log(process.cwd()); 
+    
     return createConnection(
         Object.assign(defaultOptions, {
             database:
